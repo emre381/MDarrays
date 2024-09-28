b5b619855f03b60d9fe0525a917ecefb5c3b6619
emre381 <cihatemre381@gmail.com>
2024-09-28 11:06:37 +0300

    first commit

---
+++ MDarrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDarrays
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            int[, ,] numbers = { { {2,3}, { 4, 5 } }, { {6,7 }, { 8, 9 } }, { {6,3 } , {2, 8 } } };
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    for (int z = 0; z < 2; z++)
+                    {
+                        Console.Write(numbers[x, y, z]);
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();    
+            }
+            Console.ReadLine();
+        }
+    }
+}
